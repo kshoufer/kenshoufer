92209727637d247f35c13538652bdbb607831c32
Ken Shoufer <kshoufer@gmail.com>
2014-05-26 11:40:09 -0700

    update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -34,11 +34,12 @@ if ($_SERVER['SERVER_NAME'] === "localhost") {
 } else {
     // ** MySQL settings - You can get this info from your web host ** //
     /** The name of the database for WordPress */
+    /** The name of the database for WordPress */
     define('DB_NAME', 'backcou2_kenshoufer');
     /** MySQL database username */
-    define('DB_USER', 'backcou2_bcr');
+    define('DB_USER', 'backcou2_ks');
     /** MySQL database password */
-    define('DB_PASSWORD', 'tR(SW28(P1');
+    define('DB_PASSWORD', 'belmont1');
     /** MySQL hostname */
     define('DB_HOST', 'localhost');
     /** Database Charset to use in creating database tables. */
