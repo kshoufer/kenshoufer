dd9ef0dd147c6d391b872e52705cdeb3d34164db
Ken Shoufer <kshoufer@gmail.com>
2014-05-28 18:27:29 -0700

    add grunt into workflow

---
+++ config.rb
@@ -0,0 +1,6 @@
+require 'breakpoint'
+require 'susy'
+css_dir = 'wp-content/themes/Divi-Child/assets/styles/final/'
+sass_dir = 'wp-content/themes/Divi-Child/assets/styles/final/'
+javascript_dir = 'wp-content/themes/Divi-Child/js'
+output_style = :nested
