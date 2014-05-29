3ab6593a3b89df4838c1eb028fb0c0e064e17aec
Ken Shoufer <kshoufer@gmail.com>
2014-05-28 20:37:32 -0700

    add grunt changes

--- gruntfile.js
+++ gruntfile.js
@@ -1,42 +1,49 @@
 module.exports = function(grunt) {
-	grunt.loadNpmTasks('grunt-contrib-uglify');
-	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.loadNpmTasks('grunt-contrib-compass');
-	grunt.loadNpmTasks('grunt-contrib-cssmin');
-	grunt.initConfig({
-		cssmin: {
- 			 minify: {
-    			expand: true,
-    			cwd: 'css/',
-    			src: ['*.css', '!*.min.css'],
-    			dest: 'release/',
-    			ext: '.min.css'
-  				}
-			},//cssmin
-		uglify: {
-			my_target: {
-				files: {
-					'release/script.min.js': ['components/js/script.js', 'components/js/other.js']
-				} //files
-			} //my_target
-		}, //uglify
-		compass: {
-			dev: {
-				options: {
-					config: 'config.rb'
-				}//options
-			}//dev
-		},//compass
-		watch: {
-			options: { livereload: true },
-			sass: {
-				files: ['components/sass/*.scss'],
-				tasks: ['compass:dev']
-			},//sass
-			html: {
-				files: ['*.html']
-			}//html
-		}//watch
-	})//initConfig
-	grunt.registerTask('default', 'watch');
-} //exports
+
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+    grunt.initConfig({
+
+        //concat
+        concat: {
+        options: {
+                    separator: ';',
+                },
+        dist: {
+                src: ['wp-content/themes/Divi-Child/assets/styles/build/style-header.css','wp-content/themes/Divi-Child/assets/styles/build/style-divi.css','wp-content/themes/Divi-Child/assets/styles/build/style-editor.scss'],
+                dest: 'wp-content/themes/Divi-Child/assets/styles/build/style-concat.scss',
+              },
+        },//concat
+
+        // sass
+        sass: {
+            dist: {
+                options: {
+                    sourcemap: true,
+                    style: 'expanded',
+                },
+                files: {
+                    'wp-content/themes/Divi-Child/assets/styles/build/style.css': 'wp-content/themes/Divi-Child/assets/styles/build/style-concat.scss'
+                }
+            }
+        },//sass
+
+        cssmin: {
+             minify: {
+                expand: true,
+                cwd: 'wp-content/themes/Divi-Child/assets/styles/build/',
+                src: ['*.css', '!*.min.css'],
+                dest: 'wp-content/themes/Divi-Child/',
+                ext: '.css'
+                }
+        }//cssmin
+
+
+    });
+
+    // register task
+    grunt.registerTask('default', ['concat','sass', 'cssmin']);
+
+};
